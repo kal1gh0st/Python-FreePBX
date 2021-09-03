cc041e53415078e0cfaae78b9c2aef21599104c6
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-09-03 18:19:45 +0200

    Update FreePBX-Calls-Stop.sh

--- For_Linux/FreePBX-Calls-Stop.sh
+++ For_Linux/FreePBX-Calls-Stop.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+#######################################
+#Script FreePBX0 Stop
+#######################################
+# by kal1gh0st
+
+kill $(ps aux | grep -v grep | grep "/opt/Python-FreePBX/automate.py" | awk '{print $2}')
+#questo script dovra essere schedulato nel cron, ha l'utilità di cercare il processo appeaso e killarlo basandosi sul path
 
