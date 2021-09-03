c1213ff0906fbb0024a4ee6c51fd07563a4d7e16
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-09-03 18:16:25 +0200

    Update FreePBX-Calls-Checker.sh

--- For_Linux/FreePBX-Calls-Checker.sh
+++ For_Linux/FreePBX-Calls-Checker.sh
@@ -1,2 +1,4 @@
 python /opt/Python-FREBPX/Python-FreePBX/automate.py
-pause
+#questo rimarrà in ascolto e chiamerà API 1 volta ogni 30 min
+#ovviamente ognuno dovrà inserire i propri dati e rispettivi path
+
